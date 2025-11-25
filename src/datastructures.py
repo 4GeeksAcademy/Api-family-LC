ef62b0066cdd2623d890878cab5fc2c98aa68d77
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-11-25 00:02:43 +0000

    lito para entrega

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,8 +13,23 @@ class FamilyStructure:
             {
                 "id": self._generate_id(),
                 "first_name": "John",
+                "last_name": self.last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": self.last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": self.last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
